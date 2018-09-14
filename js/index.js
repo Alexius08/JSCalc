c8574e244eb1e63b22a91f591c82df9940eb4946
Alexius08 <jrsfac@gmail.com>
2018-09-14 15:31:28 +0800

    Remove babel. Code works fine without it.

--- js/index.js
+++ js/index.js
@@ -1,65 +1,65 @@
-var selectedOp = "",canGetNextNum = false,hasError = false;
-var num = [{ "digits": 0, "decPoint": 0, "hasDecPoint": false }, { "digits": undefined, "decPoint": 0, "hasDecPoint": false }];
+let selectedOp="", canGetNextNum=false, hasError=false;
+let num=[{"digits":0,"decPoint":0,"hasDecPoint":false},{"digits":undefined,"decPoint":0,"hasDecPoint":false}];
 
-var screen = document.getElementById("screen");
-var operators = document.getElementsByClassName("op");
-var numbers = document.getElementsByClassName("num");
+const screen = document.getElementById("screen");
+const operators = document.getElementsByClassName("op");
+const numbers = document.getElementsByClassName("num");
 
-document.getElementById("removecurrent").addEventListener("click", function () {
-  if (!hasError)
-  clearcurrent();
+document.getElementById("removecurrent").addEventListener("click", () => {
+  if(!hasError)
+    clearcurrent();
 });
 
-document.getElementById("clearall").addEventListener("click", function () {
+document.getElementById("clearall").addEventListener("click", () => {
   clearcurrent();
-  Array.from(operators, function (o) {return o.style.backgroundColor = "#afa";});
+  Array.from(operators, o => o.style.backgroundColor = "#afa");
   num[1] = {
     digits: undefined,
     decPoint: 0,
-    hasDecPoint: false };
-
-  selectedOp = "";
-  hasError = false;
+    hasDecPoint: false
+  }
+  selectedOp="";
+  hasError=false;
 });
 
-document.getElementById("dot").addEventListener("click", function () {
-  if (!hasError) {
-    if (!num[0].hasDecPoint && Math.abs(num[0].digits) < Math.pow(10, 15)) {
-      num[0].hasDecPoint = true;
-      if (canGetNextNum) {
-        screen.innerHTML = "0.";
-        canGetNextNum = false;
-      } else
-      screen.innerHTML += ".";
+document.getElementById("dot").addEventListener("click", () => {
+    if(!hasError){
+      if(!num[0].hasDecPoint && Math.abs(num[0].digits)<10**15){
+        num[0].hasDecPoint=true;
+        if(canGetNextNum){
+          screen.innerHTML = "0.";
+          canGetNextNum=false;
+        }
+        else screen.innerHTML += ".";
+      }
+    }
+});
+
+document.getElementById("changesign").addEventListener("click", () => {
+  if(!hasError){
+    if(!canGetNextNum){
+      num[0].digits=-num[0].digits;
+      screen.innerHTML = num[0].hasDecPoint ? num[0].digits/(10** num[0].decPoint).toFixed(num[0].decPoint) : num[0].digits;
     }
   }
 });
 
-document.getElementById("changesign").addEventListener("click", function () {
-  if (!hasError) {
-    if (!canGetNextNum) {
-      num[0].digits = -num[0].digits;
-      screen.innerHTML = num[0].hasDecPoint ? num[0].digits / Math.pow(10, num[0].decPoint).toFixed(num[0].decPoint) : num[0].digits;
-    }
-  }
-});
-
-document.getElementById("equals").addEventListener("click", function () {
-  if (!hasError) {
-    Array.from(operators, function (o) {return o.style.backgroundColor = "#afa";});
+document.getElementById("equals").addEventListener("click", () => {
+  if(!hasError){
+    Array.from(operators, o => o.style.backgroundColor = "#afa");
     calcAnswer();
-    selectedOp = "";
-    canGetNextNum = true;
+    selectedOp="";
+    canGetNextNum=true;
   }
 });
 
-var operate = function operate() {
-  if (!hasError) {
-    Array.from(operators, function (o) {return o.style.backgroundColor = "#afa";});
+let operate = function(){
+  if(!hasError){
+    Array.from(operators, o => o.style.backgroundColor = "#afa");
     document.getElementById(this.id).style.backgroundColor = "Lime";
-    if (!canGetNextNum) {
-      if (selectedOp == "")
-      num[1] = Object.assign({}, num[0]); //num[1]=num[0] lets changes to num[0] affect num[1]
+    if(!canGetNextNum){
+      if(selectedOp=="")
+        num[1] = Object.assign({},num[0]);//num[1]=num[0] lets changes to num[0] affect num[1]
       else
         calcAnswer();
     }
@@ -68,138 +68,138 @@ var operate = function operate() {
   }
 };
 
-Array.from(operators, function (o) {return o.addEventListener("click", operate);});
+Array.from(operators, o => o.addEventListener("click", operate));
 
-var digit = function digit() {
-  if (!hasError) {
-    if (canGetNextNum) {
+let digit = function(){
+  if(!hasError){
+    if(canGetNextNum) {
       clearcurrent();
-      canGetNextNum = false;
+      canGetNextNum=false;
     }
-    if (Math.abs(num[0].digits) < Math.pow(10, 14) && num[0].decPoint < 14) {
-      num[0].digits = num[0].digits * 10 + (num[0].digits < 0 ? -parseInt(this.id) : parseInt(this.id));
-      if (num[0].hasDecPoint) {
+    if(Math.abs(num[0].digits)<10**14&&num[0].decPoint<14){
+      num[0].digits = num[0].digits*10 + ((num[0].digits<0) ? -parseInt(this.id) : parseInt(this.id));
+      if(num[0].hasDecPoint){
         num[0].decPoint++;
         screen.innerHTML += parseInt(this.id);
-      } else
-
-      screen.innerHTML = num[0].digits;
+      }
+      else
+        screen.innerHTML = num[0].digits;
     }
   }
 };
 
-Array.from(numbers, function (d) {return d.addEventListener("click", digit);});
+Array.from(numbers, d => d.addEventListener("click", digit));
 
-function clearcurrent() {
+function clearcurrent(){
   num[0] = {
     digits: 0,
     decPoint: 0,
-    hasDecPoint: false };
-
+    hasDecPoint: false
+  };
   screen.innerHTML = 0;
 }
 
-function calcAnswer() {
-  try {
-    switch (selectedOp) {
+function calcAnswer(){
+  try{
+    switch(selectedOp){
       case "subtract":
-        num[0].digits = -num[0].digits;
+        num[0].digits=-num[0].digits;
       case "add":
-        if (!num[1].hasDecPoint && !num[0].hasDecPoint) {
+        if(!num[1].hasDecPoint&&!num[0].hasDecPoint){
           num[1].digits += num[0].digits;
           console.log(num[1].digits);
-        } else
-        {
-          if (num[1].decPoint < num[0].decPoint) {
-            num[1].digits *= Math.pow(10, num[0].decPoint - num[1].decPoint);
+        }
+        else{
+          if(num[1].decPoint < num[0].decPoint){
+            num[1].digits *= (10**(num[0].decPoint-num[1].decPoint));
             num[1].decPoint = num[0].decPoint;
-          } else
-          num[0].digits *= Math.pow(10, num[1].decPoint - num[0].decPoint);
+          }
+          else num[0].digits *= Math.pow(10,(num[1].decPoint-num[0].decPoint));
           num[1].digits += num[0].digits;
-          console.log("Check if ending zeros can be shaved off");
-          while (num[1].digits % 10 == 0 && num[1].decPoint > 0) {
+          console.log("Check if ending zeros can be shaved off")
+          while(num[1].digits%10 == 0 && num[1].decPoint>0){
             num[1].decPoint--;
             num[1].digits %= 10;
           }
-          console.log((num[1].digits / Math.pow(10, num[1].decPoint)).toFixed(num[1].decPoint));
+          console.log((num[1].digits/(10 ** num[1].decPoint)).toFixed(num[1].decPoint));
         }
         break;
       case "multiply":
         num[1].digits *= num[0].digits;
         num[1].decPoint += num[0].decPoint;
-        num[1].hasDecPoint = num[1].hasDecPoint || num[0].hasDecPoint;
+        num[1].hasDecPoint = (num[1].hasDecPoint||num[0].hasDecPoint);
         break;
       case "divide":
-        if (num[0].digits == 0) throw "Division by zero";
+        if(num[0].digits==0) throw "Division by zero";
         num[1].digits /= num[0].digits;
         num[1].decPoint -= num[0].decPoint;
         console.log("Before:");
         console.log(num[1]);
-        if (num[1].digits % 1 == 0) {
-          num[1].hasDecPoint = num[1].decPoint != 0;
-          console.log("Failed to detect decimal point");
-        } else
-        {
-          if (num[1].decPoint < 0) {
+        if(num[1].digits % 1 == 0){
+          num[1].hasDecPoint = (num[1].decPoint != 0);
+          console.log("Failed to detect decimal point")
+        }
+        else{
+          if(num[1].decPoint<0){
             num[1] = {
-              digits: round(num[1].digits * Math.pow(10, -num[1].decPoint), 15),
-              decPoint: 0 };
-
+              digits: round(num[1].digits*(10 ** -num[1].decPoint),15),
+              decPoint: 0
+            }
           }
           console.log("Attempt to get digits to the right of the decimal point");
-          var ctr = 0;
-          while (Math.pow(10, ctr) * round(num[1].digits % Math.pow(10, ctr), 15) % 1 != 0 && ctr + Math.floor(Math.log10(Math.abs(num[1].digits))) + 1 - num[1].decPoint < 14) {
+          let ctr = 0;
+          while(((10**ctr * round(num[1].digits% 10**ctr ,15))%1!=0)&&(ctr+Math.floor(Math.log10(Math.abs(num[1].digits)))+1-num[1].decPoint)<14){
             ctr++;
           }
-          num[1].digits = Math.round(num[1].digits * Math.pow(10, ctr));
+          num[1].digits = Math.round(num[1].digits * 10 ** ctr);
           num[1].decPoint += ctr;
           console.log("After:");
           console.log(num[1]);
         }
-        break;}
-
+        break;
+    }
     //check if result is within allowable range
-    if (Math.abs(num[1].digits / Math.pow(10, num[1].decPoint)) > Math.pow(10, 14))
-    throw "Out of range";else
-    if (num[1].decPoint > 0) {
-      var extraDigits = 0;
-      while (Math.floor(Math.log10(num[1].digits)) - extraDigits > 14) {
+    if(Math.abs(num[1].digits/(10**num[1].decPoint))>10**14)
+      throw "Out of range";
+    else if (num[1].decPoint>0){
+      let extraDigits=0;
+      while((Math.floor(Math.log10(num[1].digits))-extraDigits)>14){
         extraDigits++;
-      }
-      num[1].digits = Math.round(num[1].digits / Math.pow(10, extraDigits));
+        }
+      num[1].digits = Math.round(num[1].digits/10**extraDigits);
       num[1].decPoint += extraDigits;
-
-      if (num[1].decPoint > 14) {
-        num[1] = {
-          digits: Math.round(num[1].digits / (Math.pow(10, num[1].decPoint) - 14)),
-          decPoint: num[1].digits == 0 ? 0 : 14 };
-
+	  
+	  if(num[1].decPoint>14){
+      num[1] = {
+        digits: Math.round(num[1].digits/(10**num[1].decPoint-14)),
+        decPoint: num[1].digits==0?0:14
       }
-      console.log(num[1].digits % 10);
-      console.log(num[1].decPoint);
-
-      var trailingZeroes = 0;
-      while (num[1].digits % Math.pow(10, trailingZeroes + 1) == 0 && trailingZeroes < num[1].decPoint) {
+	  }
+	  console.log(num[1].digits%10);
+	  console.log(num[1].decPoint);
+	  
+	  let trailingZeroes=0;
+	  while((num[1].digits%(10**(trailingZeroes+1))==0)&&(trailingZeroes<num[1].decPoint)){
         trailingZeroes++;
         console.log("Extra zero shaved off");
-      }
-      num[1].digits /= Math.pow(10, trailingZeroes);
-      num[1].decPoint -= trailingZeroes;
-
-      num[1].hasDecPoint = num[1].decPoint > 0;
+	  }
+	  num[1].digits /= 10**trailingZeroes;
+	  num[1].decPoint -= trailingZeroes;
+	  
+	  num[1].hasDecPoint = num[1].decPoint>0;
     }
   }
-  catch (err) {
-    hasError = true;
+  catch(err){
+    hasError=true;
     screen.innerHTML = err;
   }
-
-  if (!hasError) {
-    screen.innerHTML = num[1].hasDecPoint ? (num[1].digits / Math.pow(10, num[1].decPoint)).toFixed(num[1].decPoint) : num[1].digits;
+  
+  if(!hasError){
+    screen.innerHTML = num[1].hasDecPoint ? (num[1].digits/10 ** num[1].decPoint).toFixed(num[1].decPoint) : num[1].digits;
   }
 }
 
 //Jack Moore's rounding function, found at http://www.jacklmoore.com/notes/rounding-in-javascript/
-function round(value, decimals) {
-  return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+function round(value, decimals) { 
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 }
